7d78262b37dbd1ebd8f3234aaf3ffc0d3c2a18e3
boyan <boyan.soubachov@prodigyfinance.com>
2017-11-27 21:34:01 +0200

    Added part 2

---
+++ part_2/main.py
@@ -0,0 +1,26 @@
+# Some fun stuff. One of these is not like the others
+
+import random
+
+
+# get some log-normally distributed values
+def get_lognorm_values(mean, std_dev, count):
+    pass
+
+
+# find the median of some values
+def get_median(input_values):
+    pass
+
+
+# find the shortest path in a weighted graph using A*
+def find_shortest_path(input_graph):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
